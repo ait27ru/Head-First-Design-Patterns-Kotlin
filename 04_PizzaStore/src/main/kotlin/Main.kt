ab287f675a4317f02c642db3d665ecf8acafe9ce
ait27ru <ait27ru@gmail.com>
2024-02-26 12:11:19 +1300

    Change the main method so it depends on abstraction rather than concrete implementation.

--- 04_PizzaStore/src/main/kotlin/Main.kt
+++ 04_PizzaStore/src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
 import pizzaStore.ChicagoPizzaStore
 import pizzaStore.NYPizzaStore
+import pizzaStore.abstraction.PizzaStore
 
 fun main() {
-    val nyStore = NYPizzaStore()
+    val nyStore: PizzaStore = NYPizzaStore()
     nyStore.orderPizza("cheese").also { println() }
     nyStore.orderPizza("veggie").also { println() }
 
-    val chicagoStore = ChicagoPizzaStore()
+    val chicagoStore: PizzaStore = ChicagoPizzaStore()
     chicagoStore.orderPizza("cheese").also { println() }
     chicagoStore.orderPizza("clam").also { println() }
     chicagoStore.orderPizza("nonexistent").also { println() }
