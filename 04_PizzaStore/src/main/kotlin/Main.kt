84d99df5f86d42b379f1e791bd4dead7fc86d1fa
Andrei Tambovtsev <ait27ru@gmail.com>
2024-03-06 12:40:00 +1300

    Project 04_PizzaStore: add comments.

--- 04_PizzaStore/src/main/kotlin/Main.kt
+++ 04_PizzaStore/src/main/kotlin/Main.kt
@@ -3,6 +3,9 @@ import pizzaStore.NYPizzaStore
 import pizzaStore.abstraction.PizzaStore
 
 fun main() {
+    // The client doesn't care which specific factory it is.
+    // It only cares that it implements PizzaStore interface (or abstract class).
+    // Hence, the type of nyStore is an abstract PizzaStore, not concrete NYPizzaStore
     val nyStore: PizzaStore = NYPizzaStore()
     nyStore.orderPizza("cheese").also { println() }
     nyStore.orderPizza("veggie").also { println() }
